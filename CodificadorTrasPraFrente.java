b97ebad96f499e97779bcf2d0792df4cc2861b8c
Ghabriel Molina <ghabrielmolina@hotmail.com>
2025-03-19 18:19:43 -0300

    novo codificador

---
+++ CodificadorTrasPraFrente.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+
+public class CodificadorTrasPraFrente implements Codificador {
+
+  @Override
+  public String getNome() {
+    return "Codificador TrasPraFrente";
+  }
+
+  @Override
+  public LocalDate getDataCriacao() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getDataCriacao'");
+  }
+
+  @Override
+  public int getNivelSeguranca() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getNivelSeguranca'");
+  }
+
+  @Override
+  public String codifica(String str) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'codifica'");
+  }
+
+  @Override
+  public String decodifica(String str) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'decodifica'");
+  }
+
+}
