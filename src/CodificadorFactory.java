eab926383c9543c98706252bda151fcd14d5b03b
Murilo Scheffel <murilo.scheffel@outlook.com>
2025-03-19 21:47:07 +0000

    codificador factory

---
+++ src/CodificadorFactory.java
@@ -0,0 +1,14 @@
+public class CodificadorFactory {
+    public static Codificador criaCodificador(int nivelSeguranca) {
+        if(nivelSeguranca == 1) {
+            
+        }
+        if(nivelSeguranca == 2) {
+
+        }
+        if(nivelSeguranca == 3) {
+
+        }
+        return null;
+    }
+}
